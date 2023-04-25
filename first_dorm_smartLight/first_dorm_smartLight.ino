e58a42082cadb47c9e920e866094b8cf2e118f55
dongdong <joe888joejoe@gmail.com>
2023-04-25 16:36:00 +0800

    將感測器移至桌子下方，大幅調整判斷參數

--- first_dorm_smartLight/first_dorm_smartLight.ino
+++ first_dorm_smartLight/first_dorm_smartLight.ino
@@ -25,7 +25,7 @@ void setup() {
   strip.setBrightness(150); // Set BRIGHTNESS to about 1/5 (max = 255)
 }
 
-const int HIGH_VAL = 95;
+const int HIGH_VAL = 80;
 const int LOW_VAL = 55;
 int leaveTimes = 0;
 int sitDownTimes = 0;
@@ -69,11 +69,11 @@ void loop() {
     strip.show();
     WS2812_LED_control((val-LOW_VAL)*255/(HIGH_VAL - LOW_VAL));
     
-    if (val > 115){
+    if (val > 80){
         timer1 = rangeLive+1; //強迫更新資料
         sitDownTimes = 0;
         leaveTimes += 1;
-        if (leaveTimes > 2){ 
+        if (leaveTimes > 5){ 
             leaveTimes = 0;
             stateMachine = 1; //reset
             RELAY_SWITCH(false); 
@@ -85,9 +85,9 @@ void loop() {
       else lazyLED = 0;
       theaterChase(lazyLED, 240); // orange, half brightness
       timer1 = rangeLive+1;
-      if (val > 85)sitDownTimes = 0;
+      if (val > 65)sitDownTimes = 0;
       else         sitDownTimes += 1;
-      if (sitDownTimes > 3){ // 偵測到坐下 (確定觸發坐下指令)  
+      if (sitDownTimes > 1){ // 偵測到坐下 (確定觸發坐下指令)  
         sitDownTimes = 0;
         RELAY_SWITCH(true); 
       }
