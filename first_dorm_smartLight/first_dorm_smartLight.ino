666ab8540f074883484aac1e236860f8807a5fa1
dongdong <joe888joejoe@gmail.com>
2023-05-01 22:01:54 +0800

    完全新的撰寫邏輯 新增FireMode 新增Timer Interrupt概念

--- first_dorm_smartLight/first_dorm_smartLight.ino
+++ first_dorm_smartLight/first_dorm_smartLight.ino
@@ -1,160 +1,161 @@
-// This is dongdong's smart dorm arduino code
-
 #include <Adafruit_NeoPixel.h>
-const int trigPin = 10;                  //Trig Pin
+/*
+WORK_STATE
+- common
+  BRIGHTNESS 150
+- SEITCH
+  first part (0~19)
+    - Fire_Mode()
+  second part (20~LED_COUNT)
+    - My distance with screen (RED to GREEN)
+  Third part (Last 10 LEDs)
+  input tomorrow wether state
+    - RED / GREEN / BLUE
+
+SLEEP_STETE
+- commom
+  BRIGHTNESS 10
+  Fire_Mode()
+
+NEED_LIGHT_STATE
+- commom
+  BRIGHTNESS 180
+  Whhite_Mode()
+  while(long time leave) => SLEEP MODE
+*/
+#define INIT_STATE 0
+#define WORK_STATE 1
+#define SLEEP_STATE 2
+#define NEED_LIGHT_STATE 3
+
+#define TURN_ON 1
+#define TURN_OFF 0
+
+const int scanDelayTime = 60; //每次掃描間隔(ms) 250
+
+// I/O Pin
+const int trigPin = 10;                 //Trig Pin
 const int echoPin = 9;                  //Echo Pin
 const int relayPin = 8;                 //Relay Pin
 
-int scanDelayTime = 60; //每次掃描間隔(ms) 250
-// On a Trinket or Gemma we suggest changing this to 1:
-#define LED_PIN  5
-// How many NeoPixels are attached to the Arduino?
+// 定義燈條接口和燈珠數量
+#define LED_PIN 5
 #define LED_COUNT 60
+
+// 創建燈條對象
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-String inStr = "";
+
+// 設定 Timer 周期和計數器初值
+const int TIMER_PERIOD_MS = 30;
+
 void setup() {
-  inStr.reserve(500);
+  // 啟用 Timer1 中斷，並設定為 CTC 模式
+  noInterrupts();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+  OCR1A = TIMER_PERIOD_MS * 1000 / 64; // 設定比較值，使用 64 分頻
+  TCCR1B |= (1 << WGM12);
+  TCCR1B |= (1 << CS11);
+  TCCR1B |= (1 << CS10);
+  TIMSK1 |= (1 << OCIE1A);
+  interrupts();
+
+  // 初始化燈條
+  strip.begin();
+  strip.show(); // 關閉所有燈珠
+  strip.setBrightness(150); // Set BRIGHTNESS to about 1/5 (max = 255)
   Serial.begin (115200);             // Serial Port begin
+
+  // INITI/O Pin
   pinMode(relayPin, OUTPUT);      //Define inputs and outputs 
   pinMode(trigPin, OUTPUT);         
   pinMode(echoPin, INPUT);
-  RELAY_SWITCH(true);
-  /* WS2812 LED*/
-  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(150); // Set BRIGHTNESS to about 1/5 (max = 255)
+  RELAY_SWITCH(TURN_ON);
+
 }
 
-const int HIGH_VAL = 90;
-const int LOW_VAL = 50;
-int leaveTimes = 0;
-int sitDownTimes = 0;
-int noOneTimes = 0;
-int old_ColorValue = 0; // 設定目前顏色
-boolean nowStateIsLight = true;
-boolean ActiveWork = true;
-int T = 0;
-int val = 60;
-int timer1 = 1;
-int rangeLive = 200;
-int timer2 = 1;
-int rangeLive2 = 1000;
-long lazyLED = 12345000;
-int stateMachine = 1;
+int State_Machine = SLEEP_STATE;
+int LastState = State_Machine;
+int LED_RGB[3] = {0,0,0};
+
+// 定義上下界
+const int LOWER_THRESHOLD_DISTANCE = 80; // 下界設為 n 公分
+const int UPPER_THRESHOLD_DISTANCE = 100; // 上界設為 n 公分
+const int RED_DISTANCE = 55;
+const int GREEN_DISTANCE = 85;
 
 void loop() {
-  if(timer1>rangeLive){ //一段時間更新一次
-    int temp = scan();
-    if(temp < 150) val = temp;
-    timer1=0;
-  }timer1++;
-  if(timer2>rangeLive2){ //一段時間更新一次
-    timer2=0;
-  }timer2++;
-  
-  if(ActiveWork){ //判斷模式(Active: 我在位置工作 [準備偵測離開時關燈])
-    lazyLED = 12345000;
-     if(timer2 < (rangeLive2/2)){
-      for(int x=0;x<20;x+=2) strip.setPixelColor(x, strip.Color(255, 153, 18));
-      for(int x=1;x<20;x+=2) strip.setPixelColor(x, 0);
-//      if(val < 20 ){
-//        switch(stateMachine){
-//          
-//        }
-//      }
-    }else{
-      for(int x=1;x<20;x+=2) strip.setPixelColor(x, strip.Color(255, 153, 18));
-      for(int x=0;x<20;x+=2) strip.setPixelColor(x, 0);
+  int distanceVal = SCAN();
+  CHANGE_STATE(distanceVal);
+  if(LastState != State_Machine){ // run once time
+    LastState = State_Machine;
+    switch (State_Machine) {
+    case WORK_STATE:
+      RELAY_SWITCH(1);
+      break;
+    case SLEEP_STATE:
+      RELAY_SWITCH(0);
+      break;   
     }
-    strip.show();
-    WS2812_LED_control((val-LOW_VAL)*255/(HIGH_VAL - LOW_VAL));
-    
-    if (val > 105){
-        timer1 = rangeLive+1; //強迫更新資料
-        sitDownTimes = 0;
-        leaveTimes += 1;
-        if (leaveTimes > 3){ 
-            leaveTimes = 0;
-            stateMachine = 1; //reset
-            RELAY_SWITCH(false); 
-        }
-    }else leaveTimes = 0; // (0~120) 重置變數INIT value
-        
-  }else{ // not ActiveWork
-      if(lazyLED > 0) lazyLED -= 1000;
-      else lazyLED = 0;
-      theaterChase(lazyLED, 240); // orange, half brightness
-      timer1 = rangeLive+1;
-      if (val > 90 || val < 50) sitDownTimes = 0;
-      else                      sitDownTimes += 1;
-      if (sitDownTimes > 1){ // 偵測到坐下 (確定觸發坐下指令)  
-        sitDownTimes = 0;
-        RELAY_SWITCH(true); 
-      }
-      // if (sitDownTimes == 0){ // 完全沒有人坐下
-      //     noOneTimes += 1;
-      //     T = 5;
-      //     if (noOneTimes > T+1) noOneTimes = T+1;
-      // }else if (sitDownTimes > 1){ // 偵測到坐下 (確定觸發坐下指令)   
-      //     sitDownTimes = 0;
-      //     RELAY_SWITCH(true);   
-      // }else // sitDownTimes沒有持續等於0 就重置
-      //     noOneTimes = 0;
   }
-//  if (Serial.available() > 0) {
-//    inStr = Serial.readString();
-////    Serial.println(inStr.substring(0,4));
-//    if(inStr == "FUN") rainbow(1);
-//    else if(inStr == "PON") POWER_ON();
-//    else if(inStr == "POFF") POWER_OFF();
-//    else if(inStr == "ON") RELAY_SWITCH(true);
-//    else if(inStr == "OFF") RELAY_SWITCH(false);
-//    else if(inStr.substring(0,4) == "data"){ // Set and Show LED data
-////      Serial.println(inStr);
-//      inStr = inStr.substring(4);
-//      for(int i=0; i<60; i++) {
-////        if(inStr == "") break;
-//        strip.setPixelColor(i, inStr.substring(0,8).toInt());
-////        Serial.println(inStr.substring(0,8));
-//        inStr = inStr.substring(8);
-//      }
-//      strip.show(); // Update strip with new contents
-//    }
-//    else Serial.println("error command");
-//  }
+  switch (State_Machine) {
+  case WORK_STATE:
+    const int STEP = 8;
+    int MAP_VAL = (distanceVal-RED_DISTANCE)*(255-STEP)/(GREEN_DISTANCE - RED_DISTANCE);
+    MAP_VAL =  (MAP_VAL>(255-STEP)) ? (255-STEP) : (MAP_VAL < 0)? 0 : MAP_VAL;
+    if(LED_RGB[1] != MAP_VAL){
+      LED_RGB[1] = LED_RGB[1] + ((MAP_VAL > LED_RGB[1])?STEP:-1*STEP);
+      LED_RGB[0] = 255 - LED_RGB[1];
+    }
+    break;
+  }
+}
+
+// 定義 debounce 次數
+const int DEBOUNCE_COUNT = 3;
+// 宣告變數紀錄 debounce 次數
+int debounceCount = 0;
+// 宣告變數紀錄前一次判斷結果
+bool lastResult = false;
+
+void CHANGE_STATE(int distance){
+  bool result = false;
+  // 判斷是否符合上下界
+       if (distance > UPPER_THRESHOLD_DISTANCE) result = true; // 符合上界
+  else if (distance < LOWER_THRESHOLD_DISTANCE) result = false; // 符合下界
+  
+  // 當前判斷結果與前一次不同時，重置 debounceCount
+  if (result != lastResult) debounceCount = 0;
+  
+  // 當前判斷結果與前一次相同時，且 debounceCount 還沒達到設定值時，增加 debounceCount
+  if (result == lastResult && debounceCount < DEBOUNCE_COUNT) 
+    debounceCount++;
+  
+  // 當 debounceCount 達到設定值時
+  if (debounceCount == DEBOUNCE_COUNT) {
+    if (result) {
+      State_Machine = SLEEP_STATE;
+    } else {
+      State_Machine = WORK_STATE;
+    }
+    debounceCount = 0; // 重置 debounceCount
+  }
+  lastResult = result; // 更新前一次判斷結果
 }
 
 void RELAY_SWITCH(boolean mode){
-  ActiveWork = mode;
   if(mode){
     digitalWrite(relayPin, HIGH);
     POWER_ON();
-    nowStateIsLight = true;
     Serial.println("ON");
-    delay(200);
-    strip.setBrightness(160); // Set BRIGHTNESS to about 1/5 (max = 255)
+    strip.setBrightness(160);
   }else{
     POWER_OFF();
     digitalWrite(relayPin, LOW);
-    nowStateIsLight = false;
     Serial.println("OFF");
-    delay(200);
-    strip.setBrightness(10); // Set BRIGHTNESS to about 1/5 (max = 255)
+    strip.setBrightness(10);
   }
-}
-
-void WS2812_LED_control(int newValue){
-  if(newValue < 0) newValue = 0;
-  if(newValue > 255) newValue = 255;
-  int stepVal = 13;
-  if(newValue > old_ColorValue){
-    for(int i=old_ColorValue; i<newValue; i+=stepVal)
-      colorWipe(strip.Color(255-i,   i,   0), 0); // color , delay
-  }else if(newValue < old_ColorValue){
-    for(int i=old_ColorValue; i>newValue; i-=stepVal)
-      colorWipe(strip.Color(255-i,   i,   0), 0); // color , delay
-  }
-  old_ColorValue = newValue;
 }
 void POWER_ON(){
   strip.clear();
@@ -164,7 +165,6 @@ void POWER_ON(){
     strip.show();                          //  Update strip to match
     delay(5);
   }
-//  delay(500);
 }
 void POWER_OFF(){
   for(int i=59;i>=0;i--){
@@ -172,9 +172,8 @@ void POWER_OFF(){
     strip.show();                          //  Update strip to match
     delay(5);
   }
-//  delay(500);
 }
-int scan(void){
+int SCAN(void){
   long duration, cm;
   digitalWrite(trigPin, LOW);
   delayMicroseconds(5);
@@ -190,28 +189,49 @@ int scan(void){
   return cm;
 }
 
-void colorWipe(uint32_t color, int wait) {
-  for(int i=20; i<strip.numPixels(); i++) { // For each pixel in strip...
-    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
-    strip.show();                          //  Update strip to match
-    delay(wait);                           //  Pause for a moment
-  }
-}
-void theaterChase(uint32_t color, int wait) {
-  for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-    strip.clear();         //   Set all pixels in RAM to 0 (off)
-    // 'c' counts up from 'b' to end of strip in steps of 3...
-    for(int c=b; c<strip.numPixels(); c += 3) {
-      strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
+// Timer1 比較匹配中斷服務程序
+int SleepTime = 0;
+ISR(TIMER1_COMPA_vect) {
+  
+  static uint32_t last_update_time_30ms = 0;
+  static uint32_t last_update_time_SLOW = 0;
+  uint32_t current_time = millis();
+  
+  if (current_time - last_update_time_30ms >= TIMER_PERIOD_MS) {
+    last_update_time_30ms = current_time;
+    switch (State_Machine) {
+    case WORK_STATE:
+      for (int i = 20; i < LED_COUNT; i++) 
+        strip.setPixelColor(i,strip.Color(LED_RGB[0], LED_RGB[1], LED_RGB[2]));
+      // 將顏色緩存發送到燈條
+      strip.show();  
+      break;
     }
-    strip.show(); // Update strip with new contents
-    delay(wait);  // Pause for a moment
   }
-}
-void rainbow(int wait) {
-  for(long firstPixelHue = 0; firstPixelHue < 65536; firstPixelHue += 256) {
-    strip.rainbow(firstPixelHue);
-    strip.show(); // Update strip with new contents
-    delay(wait);  // Pause for a moment
+  /* Fire_Mode() */
+  if (current_time - last_update_time_SLOW >= 900) {
+    last_update_time_SLOW = current_time;
+    int USE_RANGE = 0;
+    switch (State_Machine) {
+    case WORK_STATE:
+      USE_RANGE = 20;
+      SleepTime = 0;
+      break;
+    case SLEEP_STATE:
+      USE_RANGE = LED_COUNT;
+      SleepTime++;
+      break;
+    }
+    if(SleepTime > 2000){ //After 30 min.
+      SleepTime = 2000;
+      strip.setPixelColor(random(USE_RANGE),strip.Color(0,0,0));
+    }else{
+      for(int i=0;i<USE_RANGE/4;i++){
+        strip.setPixelColor(random(USE_RANGE),strip.Color(255, 5, 5));
+        strip.setPixelColor(random(USE_RANGE),strip.Color(255, 69, 0));
+      }
+    }
+    strip.show();
   }
+
 }
